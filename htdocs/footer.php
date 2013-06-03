b6e437b263139684d58662e34af66e794dfe81f1
Ubuntu <bitnami@ip-10-243-119-32.(none)>
2013-06-03 22:12:30 +0000

    update

---
+++ htdocs/footer.php
@@ -0,0 +1,14 @@
+</div>
+<div id='footer'>
+Copyright &copy; 2013, InferLink Corporation.  All rights reserved.
+</div>
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script type="text/javascript" src="assets/js/jquery.jstree.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
