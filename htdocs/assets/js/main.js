c438171e289cff34b4a3217309f4e9b76fa5fda2
inferlink <bitnami@ip-10-243-119-32.(none)>
2013-06-07 21:55:33 +0000

    updates

--- htdocs/assets/js/main.js
+++ htdocs/assets/js/main.js
@@ -1,31 +1,14 @@
-function setImage(imgID, changeTo) {
-	// change the source of an image
-
-	current = document.getElementById(imgID).src;
-
-	if (changeTo=="plus") {
-		// change the image from a plus node to a minus node
-		// first check if this node is a last item
-		if (current.indexOf("last.png") >= 0) {document.getElementById(imgID).src = "node_pluslast.png";}
-		else {document.getElementById(imgID).src = "node_plus.png";}
-		return;
-	} else {
-		// change the image from a minus node to a plus node
-		// first check if this node is a last item
-		if (current.indexOf("last.png") >= 0) {document.getElementById(imgID).src = "node_minuslast.png";}
-		else {document.getElementById(imgID).src = "node_minus.png";}
-		return;
-	}
-}
+if (!window.location.origin)
+	window.location.origin = window.location.protocol+"//"+window.location.host;
 
 var loaded = false;
 
 function categoryClicked(id) {
 	if(cat != id && loaded) {
-		window.location.href = "http://ec2-54-243-13-79.compute-1.amazonaws.com/?cat="+id;
+		window.location.href = window.location.origin+"?cat="+id;
 	}
 	else if (loaded && id == '0' && window.location.href.indexOf("cat=0") == -1 ) {
-		window.location.href = "http://ec2-54-243-13-79.compute-1.amazonaws.com/?cat="+id;
+		window.location.href = window.location.origin+"?cat="+id;
 	}
 	loaded = true;
 }
